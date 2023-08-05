4898b4226426270b689c5b99d11eb14e6e81bebe
Aucuno024 <201204louis@gmail.com>
2023-08-05 02:09:16 +0200

    keep auxfiles option
    Add an option for keep the auxfiles folder or not in the windows

--- compile.py
+++ compile.py
@@ -1,16 +1,17 @@
 import os
 
 
-def pdflatex(file: str):
+def pdflatex(file: str, delaux: bool):
     """
-
     :param file: le chemin vers le fichier
+    :param delaux: suppression ou non des fichiers auxiliaires ?
     :return bool: le booléen correspondant à la réussite de la compilation du fichier
     """
     try:
         os.system("pdflatex -aux-directory=auxfile " + file)
-        os.system("echo O | del auxfile")
+        if delaux:
+            os.system("echo O | del auxfile")
         return True
     except BaseException:
-        print("f")
+        print("error")
         return False
--- main.py
+++ main.py
@@ -5,13 +5,23 @@ import tkinter
 
 def compilateur():
     """Fonction d'activation de la compilation"""
-    pdflatex(E.get())
+    if len(E.get()) > 0:
+        if len(L.curselection()) == 0:
+            pdflatex(E.get(), False)
+        elif L.curselection()[0] == 0:
+            pdflatex(E.get(), True)
+        else:
+            pdflatex(E.get(), False)
 
 
 window = tkinter.Tk()
 E = tkinter.Entry(window)
-f = ""
 B = tkinter.Button(window, text="Compile", command=compilateur, default="disabled")
+current_var = tkinter.StringVar()
+L = tkinter.Listbox(window, height=2, width=25)
+L.insert(1, "Supprimer fichiers auxliaires.")
+L.insert(2, "Laisser fichiers auxiliaires.")
+L.pack()
 E.pack()
 B.pack()
 window.mainloop()
